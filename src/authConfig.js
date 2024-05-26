3097c88f2e448441e91ee8a3e335f9048544b382
ts-sansten <145122062+ts-sansten@users.noreply.github.com>
2024-05-25 20:38:18 -0400

    Update authConfig.js

--- src/authConfig.js
+++ src/authConfig.js
@@ -3,7 +3,7 @@ export const msalConfig = {
     auth: {  
         clientId: "dc934073-6b26-4936-afa6-18a431aad869",  
         authority: "https://login.microsoftonline.com/233c6c53-a1d4-4e12-a0b5-a0fad87faf7a",  
-        redirectUri: "http://localhost:3000",  
+        redirectUri: "https://reskore.azurewebsites.net",  
     },  
     cache: {  
         cacheLocation: "sessionStorage",  
